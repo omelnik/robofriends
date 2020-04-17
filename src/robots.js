9df0f776a8da58f725f1b9bd008a6f0bc1ee88f8
Nik Omel <nikomel@avail-C02YV08CLVCJ.local>
2020-04-16 21:00:46 -0700

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Leanne',
+    email: 'leannegraham@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Mark Smith',
+    username: 'Mark',
+    email: 'marksmith@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'John Doe',
+    username: 'John',
+    email: 'johndoe@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Emiliy Clark',
+    username: 'Emiliy',
+    email: 'emilyclark@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Rachel Green',
+    username: 'Rachel',
+    email: 'rachel@gmail.com',
+  },
+];
