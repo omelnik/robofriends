34da1cba8997a7c3f80a9b3c623b9c612ba61e09
omelnik <nikomelf@gmail.com>
2020-04-22 18:36:15 -0700

    add more changes

--- src/contrainers/App.js
+++ src/contrainers/App.js
@@ -5,6 +5,11 @@ import Scroll from '../components/Scroll.js';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+async function fetchUsers() {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+  return resp.json();
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -19,20 +24,16 @@ class App extends Component {
   };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+    fetchUsers().then((users) => this.setState({ robots: users }));
   }
 
   render() {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
     return !robots.length ? (
-      <h1>Loading!</h1>
+      <h1 className="center">Loading!</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboBuddies</h1>
